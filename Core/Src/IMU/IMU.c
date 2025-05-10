305d7e5a3f86b4a44915601432f298aedf5465f9
adimachinelearning <duttaditya@gmail.com>
2025-05-09 20:07:10 -0400

    Added Accel read and accel calibrate functions

--- Core/Src/IMU/IMU.c
+++ Core/Src/IMU/IMU.c
@@ -6,43 +6,29 @@
   */
 void StartDefaultTask(void *argument)
 {
-	HAL_StatusTypeDef ret;
 	uint8_t buf[14];
-	buf[0] = 0x6B;
-	buf[1] = 0x01;
-	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 2, customTimeout);
-	if (ret != HAL_OK) {
-		printf("Error TX Wakeup\n");
-		osThreadTerminate(NULL);
-	}
-	osDelay(100);
-	buf[0] = REG_CONFIG;
-	buf[1] = 0;
-	buf[2] = 1 << 3;
-	buf[3] = 0;
-	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 4, customTimeout);
-	if (ret != HAL_OK) {
-		printf("Error TX Config\n");
-		osThreadTerminate(NULL);
-	}
-
-	printf("Config Wrote Successfully!\n");
-	if (CALIBRATE) {
+	configIMU(buf);
+	if (GCALIBRATE) {
 		calibrateGyro(buf);
+		osDelay(10000);
+	}
+	if (ACALIBRATE != -1) {
+		calibrateAccel(buf, ACALIBRATE);
+		osDelay(10000);
 	}
 
 	for(;;)
 	{
-		Gval vals = getGyroVals(buf);
-		printf("x:%.3f\ty:%.3f\tz:%.3f\n", vals.x, vals.y, vals.z);
+		vec3 vals = getAccelVals(buf);
+		printf("x:%.5f\ty:%.5f\tz:%.5f\n", vals.x, vals.y, vals.z);
 		osDelay(30);
 	}
 	printf("Error?\n");
 	osThreadTerminate(NULL);
 
 }
-Gval getGyroVals(uint8_t* buf) {
-	Gval vals = {0.0, 0.0, 0.0};
+vec3 getGyroVals(uint8_t* buf) {
+	vec3 vals = {0.0, 0.0, 0.0};
 	HAL_StatusTypeDef ret;
 	buf[0] = REG_VALS_START;
 	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 1, customTimeout);
@@ -54,15 +40,61 @@ Gval getGyroVals(uint8_t* buf) {
 			printf("Error RX\n");
 		} else {
 			int rawGyro = (int16_t)((buf[8] << 8) | buf[9]);
-			vals.x = (rawGyro /  65.5) - X_CALIB_OFFSET;
+			vals.x = (rawGyro /  GYRO_LSB_SENS) - GX_CALIB_OFFSET;
 			rawGyro = (int16_t)((buf[10] << 8) | buf[11]);
-			vals.y = (rawGyro /  65.5) - Y_CALIB_OFFSET;
+			vals.y = (rawGyro /  GYRO_LSB_SENS) - GY_CALIB_OFFSET;
 			rawGyro = (int16_t)((buf[12] << 8) | buf[13]);
-			vals.z = (rawGyro /  65.5) - Z_CALIB_OFFSET;
+			vals.z = (rawGyro /  GYRO_LSB_SENS) - GZ_CALIB_OFFSET;
 		}
 	}
 
 	return vals;
+}
+
+vec3 getAccelVals(uint8_t* buf) {
+	vec3 vals = {0.0, 0.0, 0.0};
+	HAL_StatusTypeDef ret;
+	buf[0] = REG_VALS_START;
+	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 1, customTimeout);
+	if (ret != HAL_OK) {
+		printf("Error TX\n");
+	} else {
+		ret = HAL_I2C_Master_Receive(&hi2c1, MPU6050_ADDR, buf, 14, customTimeout);
+		if (ret != HAL_OK) {
+			printf("Error RX\n");
+		} else {
+			int rawAccel = (int16_t)((buf[0] << 8) | buf[1]);
+			vals.x = (rawAccel /  ACCEL_LSB_SENS) - AX_CALIB_OFFSET(rawAccel /  ACCEL_LSB_SENS);
+			rawAccel = (int16_t)((buf[2] << 8) | buf[3]);
+			vals.y = (rawAccel /  ACCEL_LSB_SENS) - AY_CALIB_OFFSET(rawAccel /  ACCEL_LSB_SENS);
+			rawAccel = (int16_t)((buf[4] << 8) | buf[5]);
+			vals.z = (rawAccel /  ACCEL_LSB_SENS) - AZ_CALIB_OFFSET(rawAccel /  ACCEL_LSB_SENS);
+		}
+	}
+
+	return vals;
+}
+void configIMU(uint8_t* buf) {
+	HAL_StatusTypeDef ret;
+	buf[0] = 0x6B;
+	buf[1] = 0x01;
+	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 2, customTimeout);
+	if (ret != HAL_OK) {
+		printf("Error TX Wakeup\n");
+		osThreadTerminate(NULL);
+	}
+	osDelay(100);
+	buf[0] = REG_CONFIG;
+	buf[1] = 0;
+	buf[2] = 1 << 3;
+	buf[3] = 1 << 3;
+	ret = HAL_I2C_Master_Transmit(&hi2c1, MPU6050_ADDR, buf, 4, customTimeout);
+	if (ret != HAL_OK) {
+		printf("Error TX Config\n");
+		osThreadTerminate(NULL);
+	}
+
+	printf("Config Wrote Successfully!\n");
 }
 void calibrateGyro(uint8_t* buf) {
 	HAL_StatusTypeDef ret;
@@ -94,4 +126,90 @@ void calibrateGyro(uint8_t* buf) {
 		osDelay(50);
 	}
 	printf("\n\n\n\naverages: x:%.3f\ty:%.3f\tz:%.3f\n\n\n\n\n", xTot/1000, yTot/1000, zTot/1000);
+}
+void calibrateAccel(uint8_t* buf, int axis) {
+	int num_of_points = 0;
+	float x_sum = 0;
+	float y_sum = 0;
+	float x_squared_sum = 0;
+	float x_times_y_sum = 0;
+
+	printf("--------------------------------------------------\n");
+	printf("Orient the axis upwards against gravity\n");
+	osDelay(4000); // 1 second delay
+
+	printf("Beginning to Calibrate Part 1 (Acceleration = +1g)\n");
+
+	for (int i = 0; i < 800; i++) {
+		vec3 a = getAccelVals(buf);
+		float vals[] = {a.x, a.y, a.z};
+		float offset = vals[axis] - 1.0f;
+
+		num_of_points++;
+		x_sum += 1.0f;
+		y_sum += offset;
+		x_squared_sum += 1.0f;
+		x_times_y_sum += 1.0f * offset;
+
+		if (num_of_points % 100 == 0) {
+			printf("Still Calibrating... %d points so far\n", num_of_points);
+		}
+
+		osDelay(10); // Short delay between samples
+	}
+
+	printf("--------------------------------------------------\n");
+	printf("Orient the axis downwards against gravity\n");
+	osDelay(4000);
+
+	printf("Beginning to Calibrate Part 2 (Acceleration = -1g)\n");
+
+	for (int i = 0; i < 800; i++) {
+		vec3 a = getAccelVals(buf);
+		float vals[] = {a.x, a.y, a.z};
+		float offset = vals[axis] + 1.0f;
+
+		num_of_points++;
+		x_sum += -1.0f;
+		y_sum += offset;
+		x_squared_sum += 1.0f; // (-1)^2 = 1
+		x_times_y_sum += -1.0f * offset;
+
+		if (num_of_points % 100 == 0) {
+			printf("Still Calibrating... %d points so far\n", num_of_points);
+		}
+
+		osDelay(10);
+	}
+
+	printf("--------------------------------------------------\n");
+	printf("Orient the axis perpendicular to gravity\n");
+	osDelay(4000);
+
+	printf("Beginning to Calibrate Part 3 (Acceleration = 0g)\n");
+
+	for (int i = 0; i < 800; i++) {
+		vec3 a = getAccelVals(buf);
+		float vals[] = {a.x, a.y, a.z};
+		float offset = vals[axis];
+
+		num_of_points++;
+		// x = 0, so x_sum, x_squared_sum, x_times_y_sum don't change
+		y_sum += offset;
+
+		if (num_of_points % 100 == 0) {
+			printf("Still Calibrating... %d points so far\n", num_of_points);
+		}
+
+		osDelay(10);
+	}
+
+	// Least squares computation
+	float m = (num_of_points * x_times_y_sum - x_sum * y_sum) /
+			  (num_of_points * x_squared_sum - x_sum * x_sum);
+
+	float b = (y_sum - m * x_sum) / num_of_points;
+
+	// Output values
+	printf("m=%.5f\tb=%.5f\n", m, b);
 }
--- Core/Src/IMU/IMU.h
+++ Core/Src/IMU/IMU.h
@@ -7,7 +7,8 @@
 #include <string.h>
 #include <stddef.h>
 
-#define CALIBRATE 0
+#define GCALIBRATE 0
+#define ACALIBRATE -1
 
 static const uint8_t MPU6050_ADDR = 0b1101000 << 1;
 static const uint8_t REG_CONFIG = 0x1A;
@@ -16,18 +17,26 @@ static const uint8_t REG_ACCEL_CONFIG = 0x1C;
 static const uint8_t REG_VALS_START = 0x3B;
 static const uint32_t customTimeout = 100;
 
-#define X_CALIB_OFFSET (-4.927)
-#define Y_CALIB_OFFSET (0.455)
-#define Z_CALIB_OFFSET (0.066)
+#define GYRO_LSB_SENS (65.5)
+#define GX_CALIB_OFFSET (-4.844)
+#define GY_CALIB_OFFSET (0.246)
+#define GZ_CALIB_OFFSET (0.032)
+#define ACCEL_LSB_SENS (8192.0f)
+#define AX_CALIB_OFFSET(v) (-0.01035*(v)+0.06328)
+#define AY_CALIB_OFFSET(v) (-0.00296*(v)+0.01880)
+#define AZ_CALIB_OFFSET(v) (0.02999*(v)-0.10065)
 
 typedef struct {
 	double x;
 	double y;
 	double z;
-} Gval;
+} vec3;
 
 void StartDefaultTask(void *argument);
 void calibrateGyro(uint8_t* buf);
-Gval getGyroVals(uint8_t* buf);
+void calibrateAccel(uint8_t* buf, int axis);
+void configIMU(uint8_t* buf);
+vec3 getGyroVals(uint8_t* buf);
+vec3 getAccelVals(uint8_t* buf);
 
 #endif
